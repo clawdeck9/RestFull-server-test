305825af5683cd1f5718973787e26a6eea93d874
ClawDeck <clawdeck9@gmail.com>
2018-01-26 19:12:42 +0100

    the service is ok, better to use it with tomcat7 in Eclipse for publish pb with tomcat8

--- src/main/java/com/cluster9/rest/compte/ClientsService.java
+++ src/main/java/com/cluster9/rest/compte/ClientsService.java
@@ -11,21 +11,31 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 @Path("/clients")
 public class ClientsService {
+	
+	@GET
+	@Path("/conversion/{montant}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public double convert(@PathParam(value="montant") double mt){
+		return mt*13;
+	}
 
 	@GET
-	@Path("/accounts/{num}")
+	@Path("/accounts/{code}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public ClientAccount getAccount(@PathParam(value="num") int code){
-		return new ClientAccount(code, "Dupont", "Lille", (int)(Math.random())*100);
+	public ClientAccount getAccount(@PathParam(value="code") int code){
+		return new ClientAccount(code, "Alfred", "Maubeuge", 55);
 	}
 	
 	@GET
-	@Path("/accounts")
+	@Path("/accounts/list")
 	@Produces(MediaType.APPLICATION_JSON)
 	public ArrayList<ClientAccount> getAccountList(){
 		ArrayList<ClientAccount> list = new ArrayList<ClientAccount>();
+		int n=0;
 		for (int i=0; i<5; i++){
-			list.add(new ClientAccount((int)(Math.random())*100, "Durant", "Paris", (int)(Math.random())*100));
+			n = (int)(Math.random()*100);
+			System.out.println("age = " + n);
+			list.add(new ClientAccount(13, "Cyril", "Bois-Blancs", n ));
 		}
 		return list;
 	}
